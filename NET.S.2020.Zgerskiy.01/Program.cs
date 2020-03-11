eb985b8731061455349be260709d440dbed9056d
Zgerskiy <alex.zgerskiy@mail.ru>
2020-03-11 08:18:30 +0300

    Добавьте файлы проекта.

---
+++ NET.S.2020.Zgerskiy.01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NET.S._2020.Zgerskiy._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
